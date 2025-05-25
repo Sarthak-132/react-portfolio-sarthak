6683d8bf1c8609e3296099947bb8f86e4c6692d0
Sarthak dhoke <dksarthak101@gmail.com>
2025-05-25 15:50:59 +0530

    working on navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="logo">
+          <img src="" alt="" />
+        </div>
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Skill</a>
+          </li>
+          <li>
+            <a href="">Project</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
